6e732cda427a5a8433d7b51f241d19cad9782a95
SAGAR ROY <sagarroy761997@gmail.com>
2021-03-18 02:13:12 -0700

    Update functionExample1.c

--- functionExample1.c
+++ functionExample1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int max(int x,int y){
-	return(x>y)?x:y;
-}
+int max(int x,int y){        //function defination
+	return(x>y)?x:y;     //function defination
+}			     //function defination
 
 
 void main (void){
 	int a=100, b=200;
-	int maximum = max(a,b);
+	int maximum = max(a,b);             //function call
 	printf("MAXIMUM IS:%d", maximum);
 }
